13efa229d05a2d75e8e7f75a133ae24e9d03fba2
Tolani3 <Tolani.animasahun3@gmail.com>
2022-11-03 22:25:14 +0000

    Fix : Navbar fixed

---
+++ client/src/components/feed/Feed.jsx
@@ -0,0 +1,13 @@
+// import Post from "../post/Post";
+// import Share from "../share/Share"
+import "./feed.css"
+/* Importing the Posts array from the dummyData.js file. */
+// import {Posts} from "../../dummyData"
+
+export default function Feed() {
+  return (
+    <div>
+        feed
+    </div>
+  )
+}
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,15 +1,19 @@
 import Topbar from "../../components/topbar/Topbar";
 import Sidebar from "../../components/sidebar/Sidebar";
-// import Feed from "../../components/feed/Feed";
+import Feed from "../../components/feed/Feed";
 // import Rightbar from "../../components/rightbar/Rightbar";
-import "./home.css";
+import "./home.css"
 
 export default function Home() {
   return (
     /* A fragment. It is a way to group a list of children without adding extra nodes to the DOM. */
     <>
       <Topbar />
-      <Sidebar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        {/* <Rightbar /> */}
+      </div>
     </>
   );
 }
