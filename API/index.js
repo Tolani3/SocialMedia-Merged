669c99bf128ce72348c373a85278ce4720f386da
Tolani3 <Tolani.animasahun3@gmail.com>
2022-11-03 00:40:19 +0000

    Feature : Created post Schema, Post features such as, Create a post, Update, delete, Like & dislike, find a post, timeline posts (nested asynchronous fetch)

--- API/index.js
+++ API/index.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 
 /* Loading the environment variables from the .env file. */
@@ -27,6 +28,7 @@ app.use(morgan("common"));
 
 app.use("/api/users", userRoute);//Address for restAPI
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800,()=>{
     console.log("Backend server is running! Hello Tolani")
