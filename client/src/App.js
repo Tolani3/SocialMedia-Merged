6190786743ab041899d3666598f569e49e00260e
Tolani3 <Tolani.animasahun3@gmail.com>
2022-11-06 19:41:49 +0000

    Feature : Added secomd register and login pages, using scss too

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import Home from "./pages/home/Home";
+//import Home from "./pages/home/Home";
 //import Login from "./pages/login/Login";
 // import Profile from "./pages/profile/Profile";
 // import Register from "./pages/register/Register";
+ // import Login2 from "./pages/login2/Login2";
+  import Register2 from "./pages/register2/Register2";
+
+
 
 function App() {
-  return <Home/>;
+  return <Register2/>;
 }
 
 export default App;
