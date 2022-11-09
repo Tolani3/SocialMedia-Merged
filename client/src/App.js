766b4eadd2c78f0359dbb48a5476f357f7933fae
Tolani3 <Tolani.animasahun3@gmail.com>
2022-11-09 10:14:49 +0000

    Feature : Fixed profile page added scss to register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 //import Home from "./pages/home/Home";
 //import Login from "./pages/login/Login";
-// import Profile from "./pages/profile/Profile";
+import Profile from "./pages/profile/Profile";
 // import Register from "./pages/register/Register";
  // import Login2 from "./pages/login2/Login2";
-  import Register2 from "./pages/register2/Register2";
+  // import Register2 from "./pages/register2/Register2";
 
 
 
 function App() {
-  return <Register2/>;
+  // return <Register2/>;
+  return <Profile/>;
 }
 
 export default App;
