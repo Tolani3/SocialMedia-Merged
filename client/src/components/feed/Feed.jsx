58729711beb291a6fb3c335369de0817e8a8ee5b
Tolani3 <Tolani.animasahun3@gmail.com>
2022-11-10 11:52:37 +0000

    Chore : Installed libraries

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,20 +1,21 @@
-import { StepContext } from "@mui/material";
+//import { StepContext } from "@mui/material";
 import { useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
+import axios from "axios";
 import "./feed.css";
 /* Importing the Posts array from the dummyData.js file. */
 // import {Posts} from "../../dummyData"
 //import { useState, useEffect } from "react";
-import axios from "axios";
 
 export default function Feed() {
   const[posts,setPosts] = useState([]);
 
   useEffect(()=>{
     const fetchPosts = async () =>{
-      const res = await axios.get("posts/timeline/635351b8dadb67a384bde2d7")
-      setPosts(res.data)
+      const res = await axios.get("posts/timeline/635351b8dadb67a384bde2d7");
+      //setPosts(res.data)
+      console.log(res);
     };
     fetchPosts();
   },[]);
@@ -24,8 +25,7 @@ export default function Feed() {
     <div className="feed">
       <div className="feedWrapper">
         <Share/>
-{/*         Mapping through the Posts array and returning a Post component for each post. 
- */}        {posts.map((p) => (
+      {posts.map((p) => (
             <Post key ={p.id} post={p}/>
           ))}
       </div>
