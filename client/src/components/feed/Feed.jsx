7df94128e180a0e7a7b18f14eeeb5f3adfe24336
Tolani3 <Tolani.animasahun3@gmail.com>
2023-01-16 15:45:02 +0000

    Fixed axios error by changing "react-router-dom": "^5.2.0", Trying to get feed working

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Post from "../post/Post";
-import Share from "../share/Share"
-import "./feed.css"
+import Share from "../share/Share";
+import "./feed.css";
 import { useEffect, useState } from "react";
 /* Importing the Posts array from the dummyData.js file. */
 // import {Posts} from "../../dummyData"
@@ -14,25 +14,26 @@ export default function Feed() {
   //   console.log("Feed working Yes")
   // },[])
 
-  useEffect(()=>{
-    const fetchPosts = async () => {
-      const res = await axios.get("posts/timeline/635351b8dadb67a384bde2d7");
-      //console.log(res)
-      setPosts(res.data);
-    };
-    fetchPosts();
-  },[]);
+  useEffect(() => {
+    // const fetchPosts = async () => {
+    //  const res = await axios.get("posts/timeline/635351b8dadb67a384bde2d7")
+    const res = axios.get("posts/timeline/635351b8dadb67a384bde2d7");
+    console.log(res);
+    //   setPosts(res.data);
+    // };
+    // fetchPosts();
+  }, []);
 
   return (
     <div className="feed">
       {/* <input type="text" onChange = {e=>SetText(e.target.value)}/> */}
       <div className="feedWrapper">
         {/* Mapping through the Posts array and returning a Post component for each post. */}
-        <Share/>
+        <Share />
         {posts.map((p) => (
-            <Post key ={p.id} post={p}/>
-          ))}
+          <Post key={p.id} post={p} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
