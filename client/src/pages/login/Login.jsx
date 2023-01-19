867a3a1b43a82d07136db0173a51de63be86e6b2
Tolani3 <Tolani.animasahun3@gmail.com>
2023-01-19 17:02:40 +0000

    Getting: apiCalls.js:4 Uncaught (in promise) TypeError: dispatch is not a function

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,15 +1,24 @@
 import "./login.css";
-import { useRef } from "react";
+import { useRef, useContext } from "react";
+import { loginCall } from "../../apiCalls";
+import { AuthContext } from "../../context/AuthContext";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Login() {
-
   const email = useRef();
   const password = useRef();
+  const { user, isFetching, error, dispatch } = useContext(AuthContext);
+
   const handleClick = (e) => {
     e.preventDefault();
-    console.log(email.current.value);
+    loginCall({
+      email: email.current.value,
+      password: password.current.value,
+      dispatch,
+    });
   };
 
+  console.log(user);
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -21,7 +30,13 @@ export default function Login() {
         </div>
         <div className="loginRight">
           <form className="loginBox" onSubmit={handleClick}>
-            <input placeholder="Email" type="email" required className="loginInput" ref={email}/>
+            <input
+              placeholder="Email"
+              type="email"
+              required
+              className="loginInput"
+              ref={email}
+            />
             <input
               placeholder="Password"
               type="password"
@@ -30,10 +45,12 @@ export default function Login() {
               className="loginInput"
               ref={password}
             />
-            <button className="loginButton">Log In</button>
+            <button className="loginButton" type="submit">
+              {isFetching ? <CircularProgress color="white" size="20px"/> : "Log In"}
+            </button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">
-              Create a New Account
+            {isFetching ? <CircularProgress color="white" size="20px"/> : "Create a New Account"}              
             </button>
           </form>
         </div>
